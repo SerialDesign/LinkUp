a3707a579f8222e0abb36387aa04e67224a535fd
Daniel Burger <public@danielburger.online>
2023-04-24 20:07:27 +0200

    Clean create library screen
    Remove comments, remove dead code etc.

--- frontend/screens/createLibrary.js
+++ frontend/screens/createLibrary.js
@@ -1,108 +1,71 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import { Button, Icon } from "@rneui/themed";
-import { Input } from "@rneui/base";
-import { Alert } from "react-native";
-import { useState } from "react";
-import Header from "./Header";
+import { View } from 'react-native'
+import { Button, Icon } from '@rneui/themed'
+import { Input } from '@rneui/base'
+import { useState } from 'react'
 
 export default function CreateLibrary() {
-  const [name, setName] = useState("");
-  const [libraryName, setLibraryName] = useState("");
-  const [libraryDesc, setLibraryDesc] = useState("");
-  const [libraryLabels, setLibraryLabels] = useState("");
+  const [_name, _setName] = useState('')
+  const [libraryName, setLibraryName] = useState('')
+  const [_libraryDesc, setLibraryDesc] = useState('')
+  const [_libraryLabels, setLibraryLabels] = useState('')
 
-  const validateForm = function () {
-    Alert.alert();
-  };
-
-  const createLibrary = () => {
-    const endpointUrl = "http://localhost:8000/library/create";
+  const createLibraryHandler = () => {
+    const endpointUrl = 'http://localhost:8000/library/create'
     const payload = {
-      userId: "user12345",
-      libraryId: "example123456",
+      userId: 'user12345',
+      libraryId: 'example123456',
       libraryName: libraryName,
       links: [
         {
-          url: "url",
-          description: "desc",
+          url: 'url',
+          description: 'desc',
         },
         {
-          url: "url",
-          description: "desc",
+          url: 'url',
+          description: 'desc',
         },
         {
-          url: "url",
-          description: "desc",
+          url: 'url',
+          description: 'desc',
         },
       ],
-    };
-
-    console.log(JSON.stringify(payload));
+    }
 
     fetch(endpointUrl, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(payload),
-    }).catch((error) => {
-      console.error(error);
-    });
-  };
+    })
+  }
 
   return (
     <View>
       <View>
         <Input
-          placeholder="Name der Bibliothek"
+          placeholder='Name der Bibliothek'
           onChangeText={setLibraryName}
-          // errorStyle={{ color: "red" }}
-          // errorMessage="Pflichtfeld"
         />
-        <Input placeholder="Beschreibung" onChangeText={setLibraryDesc} />
-        <Input placeholder="#Labels" onChangeText={setLibraryLabels} />
+        <Input placeholder='Beschreibung' onChangeText={setLibraryDesc} />
+        <Input placeholder='#Labels' onChangeText={setLibraryLabels} />
 
         <Button
-          title=""
-          radius={"sm"}
-          type="solid"
-          buttonStyle={{ backgroundColor: "rgba(127, 220, 103, 1)" }}
+          title=''
+          radius={'sm'}
+          type='solid'
+          buttonStyle={{ backgroundColor: 'rgba(127, 220, 103, 1)' }}
           containerStyle={{ marginHorizontal: 30, marginVertical: 20 }}
           titleStyle={{
             marginHorizontal: 20,
-            fontWeight: "bold",
+            fontWeight: 'bold',
           }}
-          onPress={
-            createLibrary
-            // Alert.alert(name);
-            // console.warn(
-            //   "Bibliothekname: " + libraryName + ", ",
-            //   "Beschreibung: " + libraryDesc + ", ",
-            //   "Labels: " + libraryLabels
-            // );
-          }
-          //validatefForm()
+          onPress={createLibraryHandler}
         >
           Bibliothek speichern
-          <Icon name="save" color="white" />
+          <Icon name='save' color='white' />
         </Button>
       </View>
     </View>
-  );
+  )
 }
-
-// class CreateLibrary extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       name: "",
-//       desc: "",
-//       labels: "",
-//     };
-//   }
-
-//   render() {
-//     return <View></View>;
-//   }
-// }
