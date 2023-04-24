1cefc8cea57d27d55910d7c5f6515e2a2ba21d21
Daniel Burger <public@danielburger.online>
2023-04-24 19:53:28 +0200

    Adjust start app Bash script with arguments

--- README.md
+++ README.md
@@ -12,5 +12,9 @@
 
 1. Be in the root of the project
 2. Install all dependencies in both folders via `bash ./scripts/install-deps.sh`
-3. Run both the backend and the frontend via `bash ./scripts/start-app.sh`
+3. Run both the backend and the frontend via:
+
+   - 3.1. `bash ./scripts/start-app.sh -a` to start the Android app
+   - 3.2. `bash ./scripts/start-app.sh -i` to start the iOS app
+
 4. **Optional:** Reset all dependencies via `bash ./scripts/reset-deps.sh`
--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
+# Parse command-line options
+while getopts "ai" opt; do
+  case ${opt} in
+  a)
+    PLATFORM="android"
+    ;;
+  i)
+    PLATFORM="ios"
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+done
+
 # Go to backend directory
 cd backend
 
-# Start backend server via npm in the background
+# Start backend server via npm in the background and get the PID
 npm run dev &
+BACKEND_PID=$!
 
 # Go to frontend directory
 cd ../frontend
 
-# Start frontend server via npm
-npm start
+# Start frontend server via npm with the selected platform (if any)
+if [ "$PLATFORM" = "android" ]; then
+  npm run android
+elif [ "$PLATFORM" = "ios" ]; then
+  npm run ios
+else
+  npm start
+fi
+
+# Wait for user input to stop the servers
+read -p "Press any key to stop the servers..."
+
+# Kill the backend and frontend processes
+kill $BACKEND_PID
+kill $FRONTEND_PID
