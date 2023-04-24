b8889067e9b3bfb08e3f7d1ec187a528d009dade
Daniel Burger <public@danielburger.online>
2023-04-24 19:57:34 +0200

    Check if `database.json` file exists, otherwise create it

--- backend/main.js
+++ backend/main.js
@@ -1,82 +1,89 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("fs");
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
 
-const app = express();
+const app = express()
 
 // Instantiate the body-parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const DB_FILE = 'database.json'
+
+// Check if the database file exists, if not create an empty array
+if (!fs.existsSync(DB_FILE)) {
+  fs.writeFileSync(DB_FILE, '[]')
+}
 
 // create health check endpoint
-app.get("/health", (_req, res) => {
-  res.send("OK");
-});
+app.get('/health', (_req, res) => {
+  res.send('OK')
+})
 
 // Create a new library, get ID back
-app.post("/library/create", (req, res) => {
-  const newLibrary = req.body;
-  fs.readFile("database.json", (err, data) => {
+app.post('/library/create', (req, res) => {
+  const newLibrary = req.body
+  fs.readFile(DB_FILE, (err, data) => {
     if (err) {
-      console.error(err);
-      return res.status(500).send("Error reading from database");
+      console.error(err)
+      return res.status(500).send('Error reading from database')
     }
 
-    let database = JSON.parse(data);
-    console.log(newLibrary);
+    let database = JSON.parse(data)
+    console.log(newLibrary)
 
-    database.push(newLibrary);
+    database.push(newLibrary)
 
-    fs.writeFile("database.json", JSON.stringify(database), (err) => {
+    fs.writeFile(DB_FILE, JSON.stringify(database), (err) => {
       if (err) {
-        console.error(err);
-        return res.status(500).send("Error writing to database");
+        console.error(err)
+        return res.status(500).send('Error writing to database')
       }
 
-      res.send(`New library created with ID: ${newLibrary.libraryId}`);
-    });
-  });
-});
+      res.send(`New library created with ID: ${newLibrary.libraryId}`)
+    })
+  })
+})
 
 // Get a library by ID
-app.get("/library/:libraryId", (req, res) => {
-  fs.readFile("database.json", (err, data) => {
+app.get('/library/:libraryId', (req, res) => {
+  fs.readFile(DB_FILE, (err, data) => {
     if (err) {
-      console.error(err);
-      return res.status(500).send("Error reading from database");
+      console.error(err)
+      return res.status(500).send('Error reading from database')
     }
 
-    const database = JSON.parse(data);
+    const database = JSON.parse(data)
     const filteredDatabase = database.filter(
       (library) => library.libraryId === req.params.libraryId
-    );
+    )
 
     if (filteredDatabase.length === 0) {
-      return res.status(404).send("Library not found");
+      return res.status(404).send('Library not found')
     }
 
-    res.send(filteredDatabase[0]);
-  });
-});
+    res.send(filteredDatabase[0])
+  })
+})
 
 // Get all libraries by user ID
-app.get("/:userId/libraries", (req, res) => {
-  fs.readFile("database.json", (err, data) => {
+app.get('/:userId/libraries', (req, res) => {
+  fs.readFile(DB_FILE, (err, data) => {
     if (err) {
-      console.error(err);
-      return res.status(500).send("Error reading from database");
+      console.error(err)
+      return res.status(500).send('Error reading from database')
     }
 
-    const database = JSON.parse(data);
+    const database = JSON.parse(data)
     const filteredDatabase = database.filter(
       (library) => library.userId === req.params.userId
-    );
+    )
 
-    res.send(filteredDatabase);
-  });
-});
+    res.send(filteredDatabase)
+  })
+})
 
 // Start the server
 app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
-});
+  console.log('Server is listening on port 8000')
+})
